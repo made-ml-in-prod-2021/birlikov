7428b28d3685256c9df36e98813e5cd04b555ec8
shokhan <birlikoff@gmail.com>
2021-05-10 00:19:44 +0600

    rsplit instead of split in one hot col name

--- ml_project/heart_disease_classification/features/custom_ohe_transformer.py
+++ ml_project/heart_disease_classification/features/custom_ohe_transformer.py
@@ -8,7 +8,7 @@ class CustomOneHotTransformer(BaseEstimator):
         self.columns = None
         self.ohe_columns = None
 
-    def fit(self, df: pd.DataFrame):
+    def fit(self, df: pd.DataFrame, y = None):
         self.columns = df.columns.values
 
         ohe_cols = []
@@ -18,14 +18,17 @@ class CustomOneHotTransformer(BaseEstimator):
         self.ohe_columns = pd.concat(ohe_cols, axis=1).columns
         return self
 
-    def transform(self, df):
+    def transform(self, df, y = None):
         # check that we have a DataFrame with same column names as the one we fit
         if len(self.columns) != len(df.columns) or set(self.columns) != set(df.columns):
             raise ValueError('Passed DataFrame has different columns than fit DataFrame')
 
         ohe_df = pd.DataFrame(index=df.index, columns=self.ohe_columns)
         for ohe_col in ohe_df.columns:
-            col, val = ohe_col.split("_")
+            col, val = ohe_col.rsplit("_", 1)
             ohe_df[ohe_col] = df[col] == val
 
         return ohe_df
+
+    def fit_transform(self, df, y = None):
+        return self.fit(df).transform(df)
